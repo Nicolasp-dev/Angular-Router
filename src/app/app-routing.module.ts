06905990211b530cf74fc106712425902900c988
Nicolas Peña <103202106+Nicolasp-dev@users.noreply.github.com>
2023-09-20 20:01:06 -0500

    Feat: Empty paths and Root path Handling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,20 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { LoginComponent } from "./login/login.component";
 import { AboutComponent } from "./about/about.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 
 const routes: Routes = [
+  { path: "", redirectTo: "courses", pathMatch: "full" },
   { path: "login", component: LoginComponent },
   { path: "about", component: AboutComponent },
   {
-    path: "course",
+    path: "courses",
     loadChildren: () =>
       import("./courses/courses.module").then((module) => module.CoursesModule),
+  },
+  {
+    path: "**",
+    component: PageNotFoundComponent,
   },
 ];
 
