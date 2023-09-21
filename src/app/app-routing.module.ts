02dd995dc427a18d3e4b87cc1a9321d4dea5ee15
Nicolas Peña <103202106+Nicolasp-dev@users.noreply.github.com>
2023-09-20 19:37:53 -0500

    Init: Lazy loading feature

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,65 +1,21 @@
-import {NgModule} from '@angular/core';
-import {Routes, RouterModule, PreloadAllModules, UrlSerializer, NoPreloading} from '@angular/router';
-import {LoginComponent} from './login/login.component';
-import {AboutComponent} from './about/about.component';
-import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
-import {CanLoadAuthGuard} from './services/can-load-auth.guard';
-import {CustomPreloadingStrategy} from './services/custom-preloading.strategy';
-import {ChatComponent} from './chat/chat.component';
-
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { LoginComponent } from "./login/login.component";
+import { AboutComponent } from "./about/about.component";
 
 const routes: Routes = [
-    {
-      path: "",
-      redirectTo: "/courses",
-        pathMatch: "full"
-    },
-    {
-      path: "courses",
-      loadChildren: () => import('./courses/courses.module')
-                            .then(m => m.CoursesModule),
-        // canLoad: [CanLoadAuthGuard]
-       data: {
-          preload: false
-       }
-    },
-    {
-        path: "login",
-        component: LoginComponent
-    },
-    {
-        path: "about",
-        component: AboutComponent
-    },
-    {
-        path: 'helpdesk-chat',
-        component: ChatComponent,
-        outlet: 'chat'
-    },
-    {
-        path: "**",
-        component: PageNotFoundComponent
-    }
-
+  { path: "login", component: LoginComponent },
+  { path: "about", component: AboutComponent },
+  {
+    path: "course",
+    loadChildren: () =>
+      import("./courses/courses.module").then((module) => module.CoursesModule),
+  },
 ];
 
 @NgModule({
-  imports: [
-      RouterModule.forRoot(
-          routes, {
-    preloadingStrategy: CustomPreloadingStrategy,
-    scrollPositionRestoration: 'enabled',
-    paramsInheritanceStrategy: 'always',
-    malformedUriErrorHandler: (error: URIError, urlSerializer: UrlSerializer, url: string) => urlSerializer.parse("/page-not-found")
-})
-  ],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
-  providers: [
-      CanLoadAuthGuard,
-      CustomPreloadingStrategy
-  ]
+  providers: [],
 })
-export class AppRoutingModule {
-
-
-}
+export class AppRoutingModule {}
